296c4417ae04da705b0b4d52792dcb0b3f9bcde6
jeffcarbine <jeffcarbine@gmail.com>
2015-09-28 21:24:36 -0600

    Answered the questions!
    Hooray for my classmates giving me hints in their commits!

--- problems/array-sum.js
+++ problems/array-sum.js
@@ -1,7 +1,20 @@
 'use strict';
 
 function arraySum(arr) {
-
-}
+  if(arr != []) {
+    var total = 0;
+    for(var i = 0; i < arr.length; i++) {
+      if(typeof arr[i] === 'object') {
+        arr.push.apply(arr,arr[i]);
+        arr.splice(i,1);
+        i = i - 1;
+      }
+    }
+    arr.forEach(function(value){
+      total += value;
+    });
+  }
+  return total;  
+};
 
 module.exports = arraySum;
--- problems/longest-string.js
+++ problems/longest-string.js
@@ -1,7 +1,17 @@
 'use strict';
 
 function longestString(arr) {
-
+  var longest;
+  for(var i = 0; i < arr.length; i++) {
+    if(typeof arr[i] === 'string') {
+      if(longest == null) {
+        longest = arr[i];
+      } else if (longest.length < arr[i].length) {
+        longest = arr[i];
+      }
+    }
+  }
+  return longest;
 }
 
 module.exports = longestString;
